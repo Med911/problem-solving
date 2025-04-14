fabfe6096a3283082a2f0cd105871fb792590fd5
Mohamed Bouruis <bouruismohamed@gmail.com>
2025-04-14 16:47:08 +0100

    finalPrices

---
+++ LeetCode/Stack/1475. Final Prices With a Special Discount in a Shop.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+        vector<int> finalPrices(vector<int>& prices) {
+            stack<int> st;
+            int n = prices.size();
+            vector<int> res(n);
+            
+            for (int i = n - 1; i >= 0; i--) {
+                while (!st.empty() && prices[i] < st.top()) {
+                    st.pop();
+                }
+                
+                if (st.empty()) {
+                    res[i] = prices[i];
+                } else {
+                    res[i] = prices[i] - st.top();
+                }
+                
+                st.push(prices[i]);
+            }
+            
+            return res;
+        }
+    };
